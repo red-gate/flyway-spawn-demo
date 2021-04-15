9365122c166c04d810aed289547a8caf1ba40f56
cjheppell <chris.john.heppell@gmail.com>
2021-04-15 11:53:00 +0100

    tidy up github action output
    add some echos around blocks and pull the docker images but pipe
    their progress to /dev/null

--- migration-test-prod.sh
+++ migration-test-prod.sh
@@ -8,6 +8,7 @@ export PATH=$HOME/.spawnctl/bin:$PATH
 
 export SPAWN_PAGILA_IMAGE_NAME=Pagila:prod
 
+echo
 echo "Creating Pagila backup Spawn data container from image '$SPAWN_PAGILA_IMAGE_NAME'..."
 pagilaContainerName=$(spawnctl create data-container --image $SPAWN_PAGILA_IMAGE_NAME --lifetime 10m --accessToken $SPAWNCTL_ACCESS_TOKEN -q)
 
@@ -18,10 +19,18 @@ pagilaUser=$(echo $pagilaJson | jq -r '.user')
 pagilaPassword=$(echo $pagilaJson | jq -r '.password')
 
 echo "Successfully created Spawn data container '$pagilaContainerName'"
+echo
+
+docker pull postgres:12-alpine > /dev/null 2>&1
+docker pull flyway/flyway > /dev/null 2>&1
+
+echo
+echo "Starting migration of database with flyway"
 
 docker run --net=host --rm -v $PWD/sql:/flyway/sql flyway/flyway migrate -url="jdbc:postgresql://$pagilaHost:$pagilaPort/pagila" -user=$pagilaUser -password=$pagilaPassword
 
 echo "Successfully migrated 'Pagila' database"
+echo
 
 spawnctl delete data-container $pagilaContainerName --accessToken $SPAWNCTL_ACCESS_TOKEN -q
 
