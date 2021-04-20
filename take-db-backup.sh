4a2efc0d16bd09baed4d4e8c9e306cd020a07c58
Ellie <eleanor.fuller@red-gate.com>
2021-04-20 15:35:15 +0100

    Suppress spawnctl output + tidy

--- take-db-backup.sh
+++ take-db-backup.sh
@@ -3,19 +3,22 @@
 set -e
 
 echo "Downloading and installing spawnctl..."
-curl -sL https://run.spawn.cc/install | sh
+curl -sL https://run.spawn.cc/install | sh > /dev/null 2>&1
 export PATH=$HOME/.spawnctl/bin:$PATH
+echo "spawnctl successfully installed"
 
+echo
 echo "Backing up Pagila database..."
 
 mkdir backups
 
 docker run --net=host --rm -v $PWD/backups:/backups/ -e PGPASSWORD=$PAGILA_PASSWORD postgres:12-alpine pg_dump -h $PAGILA_HOST -p 5432 -U $PAGILA_USERNAME --create pagila --file /backups/pagila.sql
 
-
 echo "Creating Spawn data image..."
+echo
 
 pagilaImageName=$(spawnctl create data-image --file ./pagila-backup.yaml --lifetime 336h --accessToken $SPAWNCTL_ACCESS_TOKEN -q)
 
 echo "Successfully created Spawn data image '$pagilaImageName'"
+echo
 echo "Successfully backed up Pagila database"
